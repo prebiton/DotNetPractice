aedbfad73d8295984da187d742673a29eea93324
prebiton <48591939+prebiton@users.noreply.github.com>
2021-09-09 07:28:10 -0400

    Pattern Function

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        void paint(int stars)
+        {
+            for (int i = stars; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine(' ');
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.paint(20);
+        }
+    }
+}
