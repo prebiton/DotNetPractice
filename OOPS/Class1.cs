94877a092f3690ed9ef37aa0e1794563916a52b4
prebiton <48591939+prebiton@users.noreply.github.com>
2021-09-13 07:06:00 -0400

    Add files via upload

---
+++ OOPS/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Calculator
+    {
+        public int num1, num2;
+
+        public Calculator(int v1, int v2)
+        {
+            num1 = v1;
+            num2 = v2;
+        }
+
+        public static Calculator operator -( Calculator c1)
+        {
+            c1.num1 = -c1.num1;
+            c1.num2 = -c1.num2;
+            return c1;
+        }
+        public void print()
+        {
+            Console.WriteLine(num1.ToString()+", "+ num2.ToString());
+        }
+    }
+
+    class OperatorOverload
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(15, -25);
+            calc = -calc;
+            calc.print();
+        }
+    }
+
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            //OutDemo();
+            //RefDemo();
+        }
+
+        private static void RefDemo()
+        {
+            string str1 = "John";
+            SetValue(ref str1);
+            Console.WriteLine(str1);
+        }
+
+        private static void SetValue(ref string str1)
+        {
+            str1 = "Smith"; //only locally without ref
+        }
+
+        private static void OutDemo()
+        {
+            int result;
+            bool returnedValue = Add(25, out result); // returns 2 values
+            Console.WriteLine(result);
+        }
+
+        public static bool Add(int a, out int b)
+        {
+            // a = 20;
+            b = a + a;
+            return true;
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Shape
+    {
+        public double Width { get; set; }
+        public double Breadth { get; set; }
+    }
+    class Triangle: Shape
+    {
+        string Type;
+        static long DateOfCreation;
+        
+        static Triangle() // initialization
+        {
+            DateOfCreation = DateTime.Now.Ticks;
+            Console.WriteLine(DateOfCreation);
+        }
+
+        public Triangle(double Width1, double v2): this(Width1) // parameterized constructor
+        {
+            Width = Width1;
+            Breadth = v2;
+        }
+         public Triangle(double Width1):base() 
+        {
+            Width = Width1;
+        }
+        //public double Width { get; set; }
+        //public double Breadth { get; set; }
+    }
+    class User
+    {
+        public string name { get; set; }  // read only
+        public const int age = 10;
+        public User(string name)
+        {
+            const int age = 100; // local constant
+            //Console.WriteLine("user constructor, {0}", this.age);
+            this.name = name;
+        }
+        public virtual void Enter()
+        {
+            Console.WriteLine("1");
+        }
+    }
+
+    class Employee : User
+    {
+        public Employee(string name) : base(name)
+        {
+        }
+        public void Enter() // overriding
+        {
+            Console.WriteLine("2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BasicsOfInheritance();
+            Employee e1 = new Employee("Smith");
+            e1.Enter();
+            Console.WriteLine(e1.name);
+            User u1 = new User("John");
+            u1.Enter();
+        }
+
+        private static void BasicsOfInheritance()
+        {
+            new Shape();
+            Triangle t1 = new Triangle(12.3, 34.0); //object, reference
+            Triangle t2 = t1;
+            new Triangle(1, 2);
+            Console.WriteLine(t1);
+        }
+    }
+}
